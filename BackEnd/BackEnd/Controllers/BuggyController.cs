b307c0dc1ec4d5cd546bfa9f7f5c60eae83fe37a
Mohamed Amer <mohamed.perry351@gmail.com>
2023-01-13 23:47:48 +0200

    adding buggy controller

---
+++ BackEnd/BackEnd/Controllers/BuggyController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : ControllerBase
+    {
+        [HttpGet("not-found")]
+        public IActionResult GetNotfound()
+        {
+            return NotFound();
+        }
+        [HttpGet("bad-request")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("This is a bad request");
+        }
+
+        public IActionResult GetUnAuthrized()
+        {
+            return Unauthorized("The user is unauth");
+        }
+        [HttpGet("validation-error")]
+        public IActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem 1", "There is a problem");
+            ModelState.AddModelError("Problem 2", "There is a problem");
+            return ValidationProblem();
+        }
+
+
+
+    }
+}
